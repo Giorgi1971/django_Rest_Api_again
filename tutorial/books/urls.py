1f1da610b11250698ad14e2ca8ec726947910a78
Giorgi1971 <giooorgi7@gmail.com>
2021-03-05 19:28:48 +0400

    added books and authors also

--- tutorial/books/urls.py
+++ tutorial/books/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from books import views
 
-
 urlpatterns = [
-    # path('books/', views.book_list),
-    # path('books/<int:pk>/', views.book_detail),
+    path('books/', views.BookList.as_view()),
+    path('book/<int:pk>/', views.BookDetail.as_view()),
+    path('authors/', views.AuthorList.as_view()),
+    path('authors/<int:pk>/', views.AuthorDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- tutorial/books/views.py
+++ tutorial/books/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from books.models import Book, Author
+from books.serializers import BookSerializer, AuthorSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class AuthorList(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
